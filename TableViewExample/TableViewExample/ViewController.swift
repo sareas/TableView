39b1f2f49141b0548dc8682b33b42e9aca8b966b
Sabrina Areas <sabrinareas@gmail.com>
2015-10-27 17:18:09 -0400

    final commit

--- TableViewExample/TableViewExample/ViewController.swift
+++ TableViewExample/TableViewExample/ViewController.swift
@@ -8,8 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var myDataSOurce = ["Kwame", "Johanna", "Ethan", "Elijah", "Etienne"]
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return myDataSOurce.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        let cell = UITableViewCell()
+        cell.textLabel?.text = myDataSOurce[indexPath.row]
+        cell.detailTextLabel?.text = "detailed text"
+        return cell
+    }
 
+    @IBOutlet var myTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
